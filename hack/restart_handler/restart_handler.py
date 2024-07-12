8f6f76e6cc305ce11a1c1ad71b747195c5f2b8f6
Daniel Vega-Myhre <danielvm@google.com>
2024-07-12 05:20:08 +0000

    debugging

--- hack/restart_handler/restart_handler.py
+++ hack/restart_handler/restart_handler.py
@@ -24,7 +24,7 @@ file_handler = logging.FileHandler('restart_handler.log')  # Choose your log fil
 file_handler.setLevel(logging.DEBUG)  # Set the level for this handler
 
 # Create console handler (for writing to stdout)
-console_handler = logging.StreamHandler()  # Defaults to sys.stderr
+console_handler = logging.StreamHandler(sys.stdout)  # Defaults to sys.stderr
 console_handler.setLevel(logging.DEBUG)   # Set the level for this handler (e.g., only INFO and above)
 
 # Create formatter and add it to the handlers
